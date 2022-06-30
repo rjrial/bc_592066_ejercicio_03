2d4f1e13e05898aa89b90fe658fa44ff528b08f9
rjrial <rjrial@gmail.com>
2022-06-30 02:14:53 -0300

    Ejercicio 3 version final entregado

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let nombre
+nombre= prompt('Por favor, ingrese su nombre:')
+
+let edad
+edad= prompt('A continuación, ingrese su edad: ')
+
+if (edad<30) {
+    alert ('Hola ¿Cómo andas?')
+}
+else {
+    alert ('Bienvenido. Buenos días.')
+}
